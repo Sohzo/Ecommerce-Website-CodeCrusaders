de4ad6d9e90ffbd80bd87adedbc1c1161be07613
Blake H <blake.hollingsworth80@gmail.com>
2023-06-01 20:58:48 -0600

    changed main handlebar, added login css, fixed main page route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,22 +4,23 @@ const { Clothing } = require('../models');
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
   try {
-    const dbClothingData = await Clothing.findAll({
-      include: [
-        {
-          model: Piece,
-          attributes: ['filename', 'description'],
-        },
-      ],
-    });
+    res.render('homepage', { loggedIn: req.session.loggedIn })
+    // const dbClothingData = await Clothing.findAll({
+    //   include: [
+    //     {
+    //       model: Piece,
+    //       attributes: ['filename', 'description'],
+    //     },
+    //   ],
+    // });
 
-    const allClothing = dbClothingData.map((clothing) =>
-      clothing.get({ plain: true })
-    );
-    res.render('homepage', {
-      allClothing,
-      loggedIn: req.session.loggedIn,
-    });
+    // const allClothing = dbClothingData.map((clothing) =>
+    //   clothing.get({ plain: true })
+    // );
+    // res.render('homepage', {
+    //   allClothing,
+    //   loggedIn: req.session.loggedIn,
+    // });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -27,29 +28,29 @@ router.get('/', async (req, res) => {
 });
 
 // GET one gallery
-router.get('/clothing/:id', async (req, res) => {
-  try {
-    const dbClothingData = await Clothing.findByPk(req.params.id, {
-      include: [
-        {
-          model: Piece,
-          attributes: [
-            'id',
-            'name',
-            'price',
-            'genId',
-          ],
-        },
-      ],
-    });
+// router.get('/clothing/:id', async (req, res) => {
+//   try {
+//     const dbClothingData = await Clothing.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Piece,
+//           attributes: [
+//             'id',
+//             'name',
+//             'price',
+//             'genId',
+//           ],
+//         },
+//       ],
+//     });
 
-    const clothing = dbClothingData.get({ plain: true });
-    res.render('clothing', { clothing, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     const clothing = dbClothingData.get({ plain: true });
+//     res.render('clothing', { clothing, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 // Login route
 router.get('/login', (req, res) => {
