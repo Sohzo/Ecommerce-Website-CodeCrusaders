c10905b4b02e0b772969cbbc3490713d3cada0a2
Blake H <blake.hollingsworth80@gmail.com>
2023-06-06 11:49:09 -0600

    made button hover animation

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,4 +1,31 @@
 anime({
     targets: '#animateText',
-    translateX: 250,
+    color: ['#EDE6E3', '#DADAD9'],
+    loop: true,
+    duration: 3000,
+    direction: 'alternate',
+    easing: 'easeInOutQuad'
+});
+
+var buttons = document.querySelectorAll('button')
+
+function enterButton(element) {
+    anime({
+        targets: element,
+        scale: 1.2,
+        duration: 800
+    })
+};
+
+function exitButton(element) {
+    anime({
+        targets: element,
+        scale: 1.0,
+        duration: 600
+    })
+};
+
+buttons.forEach(btn => {
+    btn.addEventListener('mouseenter', (e) => enterButton(e.target))
+    btn.addEventListener('mouseleave', (e) => exitButton(e.target))
 });
